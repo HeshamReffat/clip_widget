af02cce6e923ba0bc1fb6977feeb0e5dd578a1ca
HishamReffat <hisham.reffat@e-motionegypt.com>
2024-02-04 12:18:52 +0200

    init

--- lib/arc_clipper.dart
+++ lib/arc_clipper.dart
@@ -11,6 +11,7 @@ class ClipWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Stack(
+      alignment: Alignment.topCenter,
       children: [
         Column(
           children: [
