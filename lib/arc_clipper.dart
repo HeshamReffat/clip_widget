d4859fe5edc3d86ccc1f59ff54c9eea75a5810a8
HishamReffat <hisham.reffat@e-motionegypt.com>
2024-02-04 14:21:14 +0200

    init

--- lib/arc_clipper.dart
+++ lib/arc_clipper.dart
@@ -3,9 +3,13 @@ import 'package:flutter/material.dart';
 import 'clipper.dart';
 
 class ClipWidget extends StatelessWidget {
+  /// the widget that will be clipped EX:Container
   final Widget child;
+  /// The space of the clip
   final double space;
+  /// The top Widget that will be inside the clip EX: CircleAvatar
   final Widget topChild;
+  ///height is use to set the top widget and the required position
   final double height;
   const ClipWidget(
       {super.key,
